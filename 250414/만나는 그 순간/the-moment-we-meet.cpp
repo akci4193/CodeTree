ce896da5b4b72505279c60e9ed4c2474266969fb
akci4193 <144820324+akci4193@users.noreply.github.com>
2025-04-14 19:18:03 +0900

    Title: 만나는 그 순간, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250414/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+#define SIZE 1000
+using namespace std;
+
+int n, m;
+char d[SIZE];
+int t[SIZE];
+char d2[SIZE];
+int t2[SIZE];
+
+int main() 
+{
+    cin >> n >> m;
+
+    int aCount[SIZE] = {};
+    int bCount[SIZE] = {};
+
+    int index = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> d[i] >> t[i];
+
+        int delta = d[i] == 'R' ? 1 : -1;
+        for (int j = 0; j < t[i]; j++)
+        {
+            if (i == 0 && j == 0)
+                aCount[index] = delta;
+            else
+                aCount[index] = aCount[index -1] + delta;
+
+            index++;
+        }
+        
+    } 
+    
+    int index2 = 0;
+    for (int i = 0; i < m; i++)
+    {
+        cin >> d2[i] >> t2[i];
+
+        int delta = d2[i] == 'R' ? 1 : -1;
+        for (int j = 0; j < t2[i]; j++)
+        {
+            if (i == 0 && j == 0)
+                bCount[index2] = delta;
+            else
+                bCount[index2] = bCount[index2 -1] + delta;
+
+            index2++;
+        }
+    }
+
+    //cout << aCount[5] << bCount[5];
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (aCount[i] == bCount[i])
+        {
+            //cout << aCount[i] << bCount[i];
+            cout << i+1;
+            return 0;
+        }
+        else if (d[i] && d2[i] == '\0')
+            break;
+        
+    } 
+
+    cout << -1;
+    
+    // Please write your code here.
+
+    return 0;
+}

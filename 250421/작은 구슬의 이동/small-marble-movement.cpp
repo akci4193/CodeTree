60aa6c668de5b54066b6ab9aa7610fd8fb477d71
akci4193 <144820324+akci4193@users.noreply.github.com>
2025-04-21 18:00:37 +0900

    Title: 작은 구슬의 이동, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250421/작은 구슬의 이동/small-marble-movement.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int n, t;
+int r, c;
+char d;
+
+int main() 
+{
+    cin >> n >> t;
+    cin >> r >> c >> d;
+
+    // Please write your code here.
+
+    int matrix[n * n] = {};
+    int dx[4] = {1, 0, -1, 0}, dy[4] = {0, -1, 0, 1};
+    int x = r-1, y = c-1;
+
+    int dirNum = -1; 
+    if (d == 'R')
+        dirNum = 0;
+    else if (d == 'D')
+        dirNum = 1;
+    else if (d == 'L')
+        dirNum = 2;
+    else
+        dirNum = 3;
+
+    for (int j = 0; j < t; j++) 
+    {
+
+        int nx = x + dx[dirNum];
+        int ny = y + dy[dirNum];
+
+        if (nx < 0 || nx >= n || ny < 0 || ny >= n) 
+        {
+            dirNum = (dirNum + 2) % 4;
+        } else 
+        {
+            x = nx;
+            y = ny;
+        }
+    }
+
+    cout << y << ' ' << x;
+
+    return 0;
+}

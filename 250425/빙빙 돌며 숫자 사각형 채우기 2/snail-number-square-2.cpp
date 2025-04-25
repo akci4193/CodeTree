fe04f55fce843f392da06bf390407a5d689e0b32
akci4193 <144820324+akci4193@users.noreply.github.com>
2025-04-25 16:32:05 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250425/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+int main() {
+    cin >> n >> m;
+
+    // Please write your code here.
+
+    
+    int dx[4] = {0, -1, 0, 1}; int dy[4] = {1, 0, -1, 0};
+    int x = 0, y = 0;
+
+    int matrix[n][m] = {};
+
+    int count = 1;
+    int dirNum = 2; 
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++) 
+        {
+            if (count +'A' -1 > 'Z')
+                count = 1;
+            matrix[x][y] = count++;
+
+            int nx = x + dx[dirNum];
+            int ny = y + dy[dirNum];
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m || matrix[nx][ny] != 0) 
+            {
+                dirNum = (dirNum + 1) % 4;
+                nx = x + dx[dirNum];
+                ny = y + dy[dirNum];
+            }
+
+            x = nx;
+            y = ny;
+
+        }
+
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++) 
+        {
+            cout << matrix[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}

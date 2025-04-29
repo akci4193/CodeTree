34d81822a95cad5ea4752ad716e8dcaadb118145
akci4193 <144820324+akci4193@users.noreply.github.com>
2025-04-29 19:21:21 +0900

    Title: 거울에 레이저 쏘기 2, Time: 68ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250429/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
+++ 250429/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -11,51 +11,76 @@ int main()
 {
     cin >> n;
 
+    int grid2[n][n] = {};
     vector<pair<pair<int,int>, int>> a;
 
     int dx[4] = {0, 1, 0, -1}; int dy[4] = {1, 0, -1, 0};
     int dirNum = 0;
+    int x = 0, y = 0;
     for (int i = 0; i < n; i++) 
     {
         for (int j = 0; j < n; j++) 
         {
             cin >> grid[i][j];
+            
+            a.push_back({{x,y}, dirNum+1});
+            int nx = x + dx[dirNum];
+            int ny = y + dy[dirNum];
 
-            if (i == 0 || i == n-1 || j ==0 || j == n-1) // 외곽
+            
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n) // 회전
             {
-                if ((i == 0 || i == n - 1) && (j == 0 || j == n - 1)) // 꼭짓점
-                {
-                    if (!(i == 0 && j == 0))
-                        a.push_back({{i,j}, dirNum});
-                    
-                    //cout << i << ' ' << j << ' ' << endl;
-                    dirNum = (dirNum+1) % 4;
-                }
-                a.push_back({{i,j}, dirNum});
+                dirNum = (dirNum + 1) % 4;
+                a.push_back({{x,y}, dirNum+1});
+                nx = x + dx[dirNum];
+                ny = y + dy[dirNum];
             }
+            //cout << nx << ' ' << ny << ' ' <<endl;
+            x = nx;
+            y = ny;
         }
     }
 
     cin >> k;
-
+    // \는 0에서는 1 , 1에서는 0 2에서는 3 3에서는 2
+    // /는 0에서는 3. 1에서는 2 2에서는 1 3에서는 0
     dirNum = a[k-1].second;
-    int x = a[k-1].first.first, y = a[k-1].first.second;    
+    x = a[k-1].first.first, y = a[k-1].first.second;    
+    //cout << x << ' ' << y << ' ' << dirNum << ' ' <<  endl;
     int count = 0;
     for (int i = 0; i < n * n; i++) 
     {
-        if (grid[x][y] == '/')
-            dirNum = (dirNum + 1) % 4;
-        else if (grid[x][y] == '\n')
-            dirNum = (dirNum + 3) % 4;
-
+        if (grid[x][y] == '\\')
+        {
+            if (dirNum == 0)
+                dirNum = 1;
+            else if (dirNum == 1)
+                dirNum = 0;
+            else if (dirNum == 2)
+                dirNum = 3;
+            else
+                dirNum = 2;
+        }
+        else if (grid[x][y] == '/')
+        {
+            if (dirNum == 0)
+                dirNum = 3;
+            else if (dirNum == 1)
+                dirNum = 2;
+            else if (dirNum == 2)
+                dirNum = 1;
+            else
+                dirNum = 0;
+        }
+            
         count++;    
 
         int nx = x + dx[dirNum];
         int ny = y + dy[dirNum];
 
+        //cout << nx << ' ' << ny << ' ' <<endl;
         if (nx < 0 || nx >= n || ny < 0 || ny >= n) 
         {
-            //cout << nx << ' ' << ny << ' ' <<endl;
             break;
         }
 

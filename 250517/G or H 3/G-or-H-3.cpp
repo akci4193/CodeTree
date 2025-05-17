56784b6201cce3ec27c6360eb9ab8189ea18d282
akci4193 <144820324+akci4193@users.noreply.github.com>
2025-05-17 15:09:42 +0900

    Title: G or H 3, Time: 17ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250517/G or H 3/G-or-H-3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+int N, K;
+int x[100];
+char c[100];
+
+int main() 
+{
+    cin >> N >> K;
+
+    int placed[10001];
+
+    int R = 0;
+
+    for (int i = 0; i < N; i++) 
+    {
+        cin >> x[i] >> c[i];
+    }
+
+    for(int i = 0; i < N; i++)
+    {
+        if (c[i] == 'G')
+            placed[x[i]] = 1;
+        else
+            placed[x[i]] = 2;
+
+        R = max(x[i], R);
+    }
+    
+    int max_cnt = 0;
+    for(int i = 1; i <= R - K + 1; i++) 
+    {
+        int num = 0;
+        for(int j = i; j <= i + K; j++)
+        {
+            num += placed[j];
+            //cout << num << ' ' << j << ' ' << endl;
+        }
+            
+    
+        max_cnt = max(max_cnt, num);
+    }
+    
+    cout << max_cnt;
+
+    // Please write your code here.
+
+    return 0;
+}
